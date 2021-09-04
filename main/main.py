d90f8188c61b3b3f6d3e554d85139641fac60969
BAHADIRking <b123qweasdzxc@gmail.com>
2021-09-04 22:39:43 +0300

    performance update

--- main/main.py
+++ main/main.py
@@ -1,17 +1,13 @@
-# import opencv
 from cv2 import cv2
 import numpy as np
 import mss
-import pyautogui
-import keyboard
-# import ai librarys
 from pynput.mouse import Button, Controller
 
-cap = cv2.VideoCapture('video9.mp4')
 
 mouse = Controller()
 stc = mss.mss()
 stc2 = mss.mss()
+acceleration_red_bar = 0
 while True:
     scr = stc.grab({
         "left": 764,
@@ -20,8 +16,6 @@ while True:
         "height": 100,
     })
 
-
-    acceleration_red_bar = 0
     frame = np.array(scr)
     hsvframe = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -64,7 +58,6 @@ while True:
             frame_red = cv2.rectangle(frame, (x1, y1), (x1 + w1, y1 + h1), (0, 0, 255), 2)
             x_red2 = int(x1 + w1 / 2)
             y_red2 = int(y1 + h1 / 2)
-            print("lolllllllllllllll",abs(acceleration_red_bar))
             acceleration_red_bar = x_red2 - acceleration_red_bar
             cv2.circle(frame, (x_red2, y_red2), 3, (0, 0, 255), -1)
             cv2.putText(frame, "hook", (x1 + w1, y1 + h1), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
@@ -76,27 +69,27 @@ while True:
                 distance = int(np.sqrt((x_red2 - x_red1) ** 2 + (y_red2 - y_red1) ** 2))
                 distance2 = int(np.sqrt((x_red2 - x_green) ** 2 + (y_red2 - y_green) ** 2))
                 cv2.putText(frame, "distance: " + str(distance), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
+                cv2.putText(frame, "distance2: " + str(distance2), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
                 cv2.putText(frame, "green bar: " + str(x_green) + " " + str(y_green), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
                             0.7, (0, 0, 255))
-                if not np.array_equal(frame_red, frame_green) and distance > 30:
+                if not np.array_equal(frame_red, frame_green) and distance > 70:
                     if x_green > x_red2 and (x_red2 < x_red1):
                         print("red bar on the left1")
                         mouse.press(Button.left)
-                    elif x_green < x_red2 and (x_red2 > x_red1)and distance > 30:
+                    elif x_green < x_red2 and (x_red2 > x_red1)and distance > 70:
                         mouse.release(Button.left)
                         print("red bar on the left2")
 
                 else:
-                        if np.array_equal(frame_red, frame_green)  and distance > distance2:
+                        if distance2 <= 10 or x_red2  > x_green and x1 > x2:
                             cv2.putText(frame, "red bar on the left", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
                             print("red bar on the left3")
-                            mouse.press(Button.left)
-
-                        else:
+                            mouse.release(Button.left)
+                        elif x_red2  < x_green and distance2 >10:
                             cv2.putText(frame, "red bar on the left", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
                                         (0, 0, 255))
                             print("red bar on the left4")
-                            mouse.release(Button.left)
+                            mouse.press(Button.left)
 
 
 
@@ -114,7 +107,7 @@ while True:
 
             x_green = int(x2 + w2 / 2)
             y_green = int(y2 + h2 / 2)
-            y_green_flag = y_green
+
             cv2.circle(frame, (x_green, y_green), 3, (0, 255, 0), -1)
             cv2.putText(frame, "green bar", (x2 + w2, y2 + h2), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0))
             cv2.putText(frame, str(x2), (80, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0))
@@ -130,6 +123,8 @@ while True:
                 pass
 
     try:
+        #The picture is divided into 10 equal parts with plenty of perpendicular lines.
+
         if np.array_equal(frame_red,frame_green):
             cv2.putText(frame, f"{np.intersect1d(frame_red, frame_green).size},1,hooked", (320, 90), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0))
         else:
